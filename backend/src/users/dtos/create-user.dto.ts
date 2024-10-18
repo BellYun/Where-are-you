14c0a08075d049c5e274d3b61b858b8259ecf55f
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-10-18 09:55:33 +0900

    feat : create-user-dto 구현

--- backend/src/users/dtos/create-user.dto.ts
+++ backend/src/users/dtos/create-user.dto.ts
@@ -1,6 +1,7 @@
 export class CreateUserDto {
   //사용자를 추가할 때 필요한 정보를 정의
   readonly name: string;
-  readonly age: number;
-  readonly breed: string;
+  readonly email: number;
+  readonly phonenumber: string;
+  readonly password: string;
 }
