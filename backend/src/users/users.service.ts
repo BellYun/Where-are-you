b72d1559c008f69b1b9f474adaa35fd15033cb3b
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-10-18 10:11:32 +0900

    feat : create 구현

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
 export class UsersService {
   //여기서 비즈니스 로직을 구현
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(user);
+  }
 }
