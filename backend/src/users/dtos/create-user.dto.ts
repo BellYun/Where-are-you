be225bcb52d8f0f9b7aa3b075536a399768dbd36
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-10-18 10:11:17 +0900

    fix : email 타입 수정

--- backend/src/users/dtos/create-user.dto.ts
+++ backend/src/users/dtos/create-user.dto.ts
@@ -1,7 +1,7 @@
 export class CreateUserDto {
   //사용자를 추가할 때 필요한 정보를 정의
   readonly name: string;
-  readonly email: number;
+  readonly email: string;
   readonly phonenumber: string;
   readonly password: string;
 }
