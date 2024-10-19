742b9bc86100dd7ed583600f1e4b77e9c62e9d40
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-10-19 23:29:06 +0900

    fix : 유저 생성이 회원가입을 사용하도록 수정

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { signupDto } from 'src/auth/dtos/singup.dto';
 
 @Injectable()
 export class UsersService {
@@ -12,8 +13,13 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
-  create(createUserDto: CreateUserDto): Promise<User> {
-    const user = this.usersRepository.create(createUserDto);
+  create(signupDto: signupDto): Promise<User> {
+    const user = this.usersRepository.create({
+      name: signupDto.name,
+      email: signupDto.email,
+      password: signupDto.password,
+      phoneNumber: signupDto.phonenumber
+    });
     return this.usersRepository.save(user);
   }
 
